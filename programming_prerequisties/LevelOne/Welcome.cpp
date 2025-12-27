3c843c60b8d37552f69276b38c89f458faedd876
priyanshusingh0023 <priyanshusingh7647@gmail.com>
2025-12-27 19:46:15 +0530

    [priyanshu singh]: Added my first welcome program on github

---
+++ programming_prerequisties/LevelOne/Welcome.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+void Welcome( string name){
+    cout<<name<<" Welcome To the World of Coding";
+}
+int main(){
+string name;
+cout<<"Please Enetr your name here :";
+cin>>name;
+Welcome(name);
+return 0;
+}

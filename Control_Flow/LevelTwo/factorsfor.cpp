aec03b9a2062a8f7b49e30fbf386f9e157a6983b
priyanshusingh0023 <priyanshusingh7647@gmail.com>
2026-01-03 19:54:59 +0530

    [pr]:

---
+++ Control_Flow/LevelTwo/factorsfor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void printFactors(int number) {
+    cout << "Factors of " << number << " are: ";
+    for (int i = 1; i <= number; i++) {
+        if (number % i == 0) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int number;
+    cout << "Enter a positive integer: ";
+    cin >> number;
+
+    if (number <= 0) {
+        cout << "Please enter a positive integer!" << endl;
+    } else {
+        printFactors(number);
+    }
+
+    return 0;
+}
